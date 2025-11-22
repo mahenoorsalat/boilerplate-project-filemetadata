ff654df13e0c4fd43af86140c8bb2e468b2c8cdd
mahenoorsalat <salatmahenoor7.8.6@gmail.com>
2025-11-22 13:02:03 +0530

    added

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
+
 require('dotenv').config()
 
+
 var app = express();
+var upload = multer({ dest: 'uploads/' });
+var port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +16,22 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse" , upload.single("upfile") ,  async(req, res) =>{
+  if(!req.file){
+        return res.json({ error: 'No file uploaded' });
+
+  }
+  return res.json({
+    name:req.file.originalname , 
+    type : req.file.mimetype , 
+    size : req.file.size
+  })
+
+  
+})
 
 
 
-const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
